fa5caac87f947b907a78f51f20e615bad6c85885
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2022-05-12 17:20:31 +0300

    export commit

---
+++ src/main/java/ru/opgmap/opgmap_geteway/OpgmapGetewayApplication.java
@@ -0,0 +1,13 @@
+package ru.opgmap.opgmap_geteway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpgmapGetewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpgmapGetewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/opgmap/opgmap_geteway/OpgmapGetewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.opgmap.opgmap_geteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpgmapGetewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
