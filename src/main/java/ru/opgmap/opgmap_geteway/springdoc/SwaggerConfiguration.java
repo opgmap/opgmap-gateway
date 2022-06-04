9b3a4ff81d911592d9028e723c62372938ed0d99
Dinar Shagaliev <dinar.shagaliev2002@gmail.com>
2022-06-04 12:46:02 +0300

    added docker profile

---
+++ src/main/java/ru/opgmap/opgmap_geteway/springdoc/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package ru.opgmap.opgmap_geteway.springdoc;
+
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfiguration {
+
+    @Bean
+    public CommandLineRunner openApiGroups(
+            RouteDefinitionLocator locator,
+            SwaggerUiConfigParameters swaggerUiParameters) {
+        return args -> locator
+                .getRouteDefinitions().collectList().block()
+                .stream()
+                .map(RouteDefinition::getId)
+                .filter(id -> id.matches(".*-service"))
+                .map(id -> id.replace("-service", ""))
+                .forEach(swaggerUiParameters::addGroup);
+    }
+
+}
